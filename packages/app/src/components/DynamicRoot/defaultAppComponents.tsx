58d66353c05c1e23efd40b5c90a6ab5a148519c3
gustavolira <gustavolira@users.noreply.github.com>
2024-03-11 20:15:59 +0000

    Automated deployment: Mon Mar 11 20:15:59 UTC 2024 a187a0090a1319e5d98e5c59c94cad7422a63ba7

--- packages/app/src/components/DynamicRoot/defaultAppComponents.tsx
+++
@@ -1,9 +0,0 @@
-import { AppComponents } from '@backstage/core-plugin-api';
-import React from 'react';
-import { SignInPage } from '../SignInPage/SignInPage';
-
-const defaultAppComponents: Partial<AppComponents> = {
-  SignInPage: props => <SignInPage {...props} />,
-};
-
-export default defaultAppComponents;
